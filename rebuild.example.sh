e968d8306bef29d22454123bae667a0400c4525b
johan.v.gustafsson <johan.v.gustafsson@husqvarnagroup.com>
2021-03-24 19:54:27 +0100

    Add rebuild shellscript

---
+++ rebuild.example.sh
@@ -0,0 +1,20 @@
+options_file="dist/options/json/options.json"
+temp_file="./options.json"
+copy=0
+
+if [ -f "${options_file}" ]
+then
+  cp $options_file $temp_file
+  copy=1
+fi
+
+rm -rf dist
+rm  ~/bitbar-scripts/plugins/cryptocurrency-prices.2m.js
+yarn build
+chmod +x dist/main.2m.js
+ln -s dist/main.2m.js ~/bitbar-scripts/plugins/cryptocurrency-prices.2m.js
+
+if [ $copy  == 1 ]
+then
+  mv $temp_file $options_file
+fi
